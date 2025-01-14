6b004ddfc3d25e512502695cc1f2a9e97c0a7201
h-schroeder-coder <hankorschroeder@gmail.com>
2025-01-14 10:20:20 -0600

    added netlify link

--- src/Footer.js
+++ src/Footer.js
@@ -7,22 +7,35 @@ export default function Footer() {
     <div className="Footer">
       <footer>
         This webpage was coded by
-        <a href="https://github.com/h-schroeder-coder" target="_blank" rel="noreferrer">
+        <a
+          href="https://github.com/h-schroeder-coder"
+          target="_blank"
+          rel="noreferrer"
+        >
           Hannah Schroeder
         </a>
         as part of a
-        <a href="https://www.shecodes.io/workshops" target="_blank" rel="noreferrer">
+        <a
+          href="https://www.shecodes.io/workshops"
+          target="_blank"
+          rel="noreferrer"
+        >
           SheCodes Plus Workshop,
         </a>
         and is on
         <a
           href="https://github.com/h-schroeder-coder/shecodes_react_wk4_1"
-          target="_blank" rel="noreferrer"
+          target="_blank"
+          rel="noreferrer"
         >
           GitHub
         </a>
         and is hosted by
-        <a href="/" target="_blank" rel="noreferrer">
+        <a
+          href="https://shecodes-react-wk-4-1.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
           Netifly
         </a>
       </footer>
